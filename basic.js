b7e667b19e8c64a2bde4c180168c1b17115eedb3
LayGomez <laylamelanygomez@gmail.com>
2024-10-07 12:31:04 +0200

    ejercicio 11 y 12

--- basic.js
+++ basic.js
@@ -123,7 +123,7 @@ console.log ("valor 1 es distinto que valor 2: "+ comparar2)
 let num1 = 15
 let num2 = 20
 
-let comparision = //Realiza la operación de comparación que consideres para que el resultado en consola sea true
+let comparision = num1 <= num2;//Realiza la operación de comparación que consideres para que el resultado en consola sea true
 console.log(comparision)
 
 //Ejercicio 12: completa el ejercicio
@@ -131,7 +131,7 @@ console.log(comparision)
 let num3 = 1
 let num3AsString = "1"
 
-let result = //Realiza la operación de comparación que consideres entre las dos variables dadas, para que el resultado en consola sea false sin usar el comparador de desigualdad
+let result = num3 === num3AsString;//Realiza la operación de comparación que consideres entre las dos variables dadas, para que el resultado en consola sea false sin usar el comparador de desigualdad
 console.log(result) 
 
 //Ejercicio 13: completa el ejercicio
